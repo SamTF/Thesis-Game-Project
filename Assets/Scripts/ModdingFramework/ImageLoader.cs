fcfa233975291c8dce561250a9f2ad73c6decab9
Sam :D <sebastiancc.info@gmail.com>
2023-02-13 14:04:23 +0100

    Created component that allows for users to modify any sprite

---
+++ Assets/Scripts/ModdingFramework/ImageLoader.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.IO;
+
+public static class ImageLoader
+{
+    // CONSTANTS
+    private const float pixelsPerUnit = 16f;
+    private static Vector2 pivot =  new Vector2(0.5f, 0.5f);
+    private static Vector2 pivotBL = new Vector2(0f, 0f);
+
+    /// <summary>
+    /// Loads a custom image from disk and creates a Sprite Object from it.
+    /// Optionally you can specify an exact size that the custom image must have.
+    /// </summary>
+    public static Sprite LoadSprite(string filename) {
+        Texture2D spriteTexture = LoadTextureFromFile(filename);    // Loads the requested image file as a Texture2D Object
+        if (spriteTexture == null)  return null;                    // Checks if it was successful
+
+        // Creating the Sprite
+        Sprite newSprite = Sprite.Create(
+            spriteTexture,
+            new Rect(0, 0, spriteTexture.width, spriteTexture.height),
+            pivot,
+            pixelsPerUnit
+        );
+
+        return newSprite;
+    }
+
+
+    /// <summary>
+    /// Loads a PNG or JPG file from disk into a Texture2D Object.
+    /// Returns null if loading fails
+    /// </summary>
+    private static Texture2D LoadTextureFromFile(string filename) {
+        string filePath = $"{ModManager.ModDirectory}/{filename}";
+
+        // Checking if the file exists
+        if (!File.Exists(filePath)) {
+            Debug.LogError($"No such image: {filePath}");
+            return null;
+        }
+
+        // Creating the texture
+        byte[] fileData = File.ReadAllBytes(filePath);  // reading the Image's data into an array of bytes
+        Texture2D texture = new Texture2D(1,1);         // creating a Texture2D object to house it
+        texture.filterMode = FilterMode.Point;          // Setting the correct filter mode (no filtering)
+
+        // checks if the data is readable
+        if (!texture.LoadImage(fileData)) {     
+            return null;                                // return null is image is unreadable
+        }
+
+        // Return the texture if everything was successful
+        return texture; 
+    }
+}
---
+++ Assets/Scripts/ModdingFramework/ModdableSprite.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A GameObject with this component can have its Sprite changed by the player in the mod directory.
+/// </summary>
+public class ModdableSprite : MonoBehaviour
+{
+    [SerializeField][Tooltip("Does this gameObject's sprite need to have a specific size?")]
+    private bool enforceSize = true;
+    [SerializeField][Tooltip("What should the image file for this sprite be called? Default: The GameObject's name")]
+    private string modFileName = null;
+    private string modFileType = "png";
+
+    private SpriteRenderer spriteRenderer = null;
+
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        // Setting the name of the file required to mod the image
+        string name = modFileName != "" ? modFileName : this.name;
+        modFileName = $"{name}.{modFileType}";
+
+        // Checking if the player created a custom asset for this GameObject
+        if (ModManager.ModExists(modFileName)) {
+            Debug.Log($"[{this.name}] >>> Player created custom sprite for [{modFileName}]");
+            ReplaceSprite();
+        }
+    }
+
+    private void ReplaceSprite() {
+        // Vector2Int spriteSize = new Vector2Int((int)sr.sprite.rect.width, (int)sr.sprite.rect.height);  // getting the size of the sprite within the texture atlas: https://answers.unity.com/questions/1489211/getting-sprite-heightwidth-in-local-space.html
+        Sprite modSprite = ImageLoader.LoadSprite(modFileName); // Loading custom asset into a Sprite
+
+        if (modSprite == null)  return;                         // checking if a sprite was successfully found
+
+        spriteRenderer.sprite = modSprite;                      // replacing current sprite with custom asset
+    }
+}
