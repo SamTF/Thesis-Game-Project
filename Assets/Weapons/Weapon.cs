68f39c1528ee641453f5fa5f166001d735635ff9
Sam :D <sebastiancc.info@gmail.com>
2023-02-24 00:31:51 +0100

    Added TBOI-style direction shooting offset!!

--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -51,19 +51,21 @@ public class Weapon : MonoBehaviour
     /// </summary>
     /// <param name="direction">Vector2 (Up, Down, Left, or Right)</param>
     private void Shoot(Direction direction) {
-        // Direction the player is shooting in
-        Vector2 shootingVector = GetDirectionVector(direction);
+        // Direction for the projectile to move in
+        Vector2 shootingVector = GetShootingVector(direction);
 
         // Direction the player is moving in
         Vector2 movementVector = input.Movement;
         
-
         // Checking if moving and shooting in the same direction
         float speed = shotSpeed;
         if (movementVector == shootingVector) {
             speed *= 1.5f;
         }
 
+        Debug.Log(shootingVector);
+
+        // Getting the appropriate spawn point for the projectile given the aiming direction
         Vector2 shootPoint = shootPoints.Direction2ShootPoint(direction);
 
         // Instantiate the Projectile
@@ -99,6 +101,51 @@ public class Weapon : MonoBehaviour
             default:
                 return Vector2.right;
         }
+    }
+
+    /// <summary>
+    /// Gets the Vector2 for the projectile direction, offset by the player's movement direction.
+    /// Inspired by TBOI's shooting mechanics :)
+    /// </summary>
+    /// <param name="direction">Direction in which the player is aiming at</param>
+    /// <returns>Vector2 for the projectile's movement direction.</returns>
+    private Vector2 GetShootingVector(Direction direction) {
+        // Direction the player is moving in
+        Vector2 movementVector = input.Movement;
+
+        // Direction the player is shooting in
+        Vector2 shootingVector = GetDirectionVector(direction);
+        // these variables need a lot of tweaking but it works perfectly!!!
+
+        // IF SHOOTING HORIZONTALLY WHILE MOVING VERTICALLY
+        if (shootingVector.x != 0 && movementVector.y != 0) {
+            // checking for a minimum time to hold direction before applying offset
+            if (input.TimeHoldingDirection[0] < 0.15f) {
+                return shootingVector;
+            };
+
+            // creating the offset vector
+            float offset = input.TimeHoldingDirection[0] * 0.5f;
+            offset = Mathf.Clamp(offset, 0.25f, 0.75f);
+            Debug.Log(offset);
+            shootingVector.y = movementVector.y * offset;
+        }
+        // SHOOTING VERTICALLY WHILE MOVING HORIZONTALLY
+        else if (shootingVector.y != 0 && movementVector.x !=0 ) {
+            // checking for a minimum time to hold direction before applying offset
+            if (input.TimeHoldingDirection[0] < 0.15f) {
+                return shootingVector;
+            }
+
+            // creating the offset vector
+            float offset = input.TimeHoldingDirection[0] * 0.5f;
+            offset = Mathf.Clamp(offset, 0.25f, 1f);
+            Debug.Log(offset);
+            shootingVector.x = movementVector.x * offset;
+        }
+
+        // Return the vector after all these calculations
+        return shootingVector;
     }
 
 
