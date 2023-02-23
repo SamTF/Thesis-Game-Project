a3916919b6bf259e6a81a0cd005c8de38855a109
Sam :D <sebastiancc.info@gmail.com>
2023-02-23 13:14:00 +0100

    Updated player prefab and camera size

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -8,8 +8,14 @@ public class Player : MonoBehaviour
     private Rigidbody2D     rb              = null;
     private InputManager    input           = null;
     private Stats           stats           = null;
-    private GameObject      spriteObject    = null;
     private Status          status          = null;
+
+    // Children
+    [SerializeField]
+    private Transform spriteObject = null;
+    [SerializeField]
+    private Transform shadow = null;
+
 
 
     private void Awake()
@@ -17,8 +23,10 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         input = GetComponent<InputManager>();
         stats = GetComponent<Stats>();
-        spriteObject = GameObject.Find("Sprite");
         status = GetComponent<Status>();
+
+        spriteObject = spriteObject ? spriteObject : transform.Find("Sprite");
+        shadow = shadow ? shadow : transform.Find("Shadow");
     }
 
 
@@ -26,6 +34,9 @@ public class Player : MonoBehaviour
     public Rigidbody2D RigidBody => rb;
     public InputManager Input => input;
     public Stats Stats => stats;
-    public Transform SpriteObject => spriteObject.transform;
     public Status Status => status;
+
+    // Child getters
+    public Transform SpriteObject => spriteObject;
+    public Transform Shadow => shadow;
 }
