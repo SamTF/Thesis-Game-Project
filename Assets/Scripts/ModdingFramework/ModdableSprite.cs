ab203e260c612a3f7df192b4acdafec8d8f5c766
Sam :D <sebastiancc.info@gmail.com>
2023-03-02 13:47:06 +0100

    Moved this to Start again
    Otherwise it wouldn't fetch the stats rom the default image in the build, for some reason...

--- Assets/Scripts/ModdingFramework/ModdableSprite.cs
+++ Assets/Scripts/ModdingFramework/ModdableSprite.cs
@@ -24,7 +24,7 @@ public class ModdableSprite : MonoBehaviour
     private SpriteRenderer spriteRenderer = null;
 
 
-    private void Awake() {
+    private void Start() {
         // Checking whether to get sprite render from this component or from a child object
         if (spriteIsChild) {
             spriteRenderer = GetComponentInChildren<SpriteRenderer>();
@@ -47,6 +47,7 @@ public class ModdableSprite : MonoBehaviour
         // Also analyse stats for Default image
         } else {
             if (!analyseImage)  return;
+            Debug.Log("[ModdableSprite] >>> Analysing Stats for default image...");
             Texture2D defaultTex = spriteRenderer.sprite.texture;
             Colour[] colours = ImageAnalyser.Analyse(defaultTex);
 
