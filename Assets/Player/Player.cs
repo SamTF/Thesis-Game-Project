16ff784dfe3f7d5c77225a40ec3c06274efd9f76
Sam :D <sebastiancc.info@gmail.com>
2023-03-30 14:44:48 +0200

    Added collision checking and knockback effect on hit

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     private CircleCollider2D collider       = null;
     private Health          health          = null;
     private Animator        animator        = null;
+    private HitStop         hitStop         = null;
 
     // Children
     [SerializeField]
@@ -30,9 +31,42 @@ public class Player : MonoBehaviour
         collider = GetComponent<CircleCollider2D>();
         health = GetComponent<Health>();
         animator = GetComponent<Animator>();
+        hitStop = GetComponentInChildren<HitStop>();
 
         spriteObject = spriteObject ? spriteObject : transform.Find("Sprite");
         shadow = shadow ? shadow : transform.Find("Shadow");
+    }
+
+
+    // Checking for collisions
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(((1<<other.gameObject.layer) & health.DamageLayer) != 0) {
+            Debug.Log($"[HEALTH] >>> Ouch! Player was damaged by {other.name}");
+            OnHit(other.transform);
+        }
+    }
+
+    /// <summary>
+    /// Triggers when the Player is hit and takes damage.
+    /// <param name="hitter">The Object that hit the player</param>
+    /// </summary>
+    private void OnHit(Transform hitter) {
+        // Checking if the player can take damage.
+        if (status.IsInvulnerable) {
+            Debug.Log("Player can't take damage right now. They are invulnerable!");
+            return;
+        }
+
+        // Take Damage on the Health component
+        health.TakeDamage();
+
+        // Hit Stop! Freeze frame! Game feel! Oh yeah!
+        hitStop.Hit();
+        
+        // Knockback away from the damage source
+        status.IsKnockedBack = true;
+        Vector2 knockbackVector = Vector2Int.RoundToInt((transform.position - hitter.position).normalized);
+        rb.AddForce(knockbackVector * 800f);
     }
 
 
