8046354e769bc7ee10e73135d0a9773668fbdedd
Sam :D <sebastiancc.info@gmail.com>
2023-04-13 16:33:59 +0200

    Added summary description

--- Assets/Enemies/ScreenBouncer/Bouncer.cs
+++ Assets/Enemies/ScreenBouncer/Bouncer.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Melee enemy that travels across Screen Space and bounces around the Screen edges.
+/// </summary>
 public class Bouncer : Enemy
 {
     [Header("Screen Bouncer")]
