8f216a75630a2827f809db982141add83e17cc61
Sam :D <sebastiancc.info@gmail.com>
2023-04-18 15:05:38 +0200

    Added telegraph animation and randomised shoot delay

--- Assets/Enemies/Shooter/Shooter.cs
+++ Assets/Enemies/Shooter/Shooter.cs
@@ -25,40 +25,47 @@ public class Shooter : Enemy
     [SerializeField][Tooltip("How many units the projectile travels before being affected by gravity")][Range(5,15)]
     private int projectileRange = 10;
 
-    [SerializeField]
+    [SerializeField][Tooltip("Whether this enemy is allowed to Shoot projectile while moving")]
     private bool canShootWhileMoving = false;
-
+    private bool canShootWhileRunningAway = false;
 
     // Components
     private ShooterMovement myMovement = null;
 
-
+    
+    // Start shooting behaviour
     private void Start() {
         myMovement = GetComponent<ShooterMovement>();
         
-        StartCoroutine(ShootBehaviour());
+        StartCoroutine(ShootBehaviour(1f));
     }
-
 
     /// <summary>
     /// Starts the shooting pattern for this enemy.
     /// </summary>
-    /// <returns></returns>
-    private IEnumerator ShootBehaviour() {
-        while (true) {
-            yield return new WaitForSeconds(shootingInterval);
+    /// <param name="startDelay">How many seconds before the shooting behaviour begins</param>
+    private IEnumerator ShootBehaviour(float startDelay=1f) {
+        // Add some randomised delay (if delay is wanted) so that enemies don't all shoot at the exact same time
+        if (startDelay > 0f) startDelay += Random.Range(0f, 2f);
+        yield return new WaitForSeconds(startDelay);
 
+        while (true) {
+            yield return new WaitForSeconds(0.1f);
+            
             // Don't shoot while moving or running away
-            if
-            (
-                !canShootWhileMoving &&
-                (myMovement.IsMoving || myMovement.IsRunningAway)
-            )
-                continue;
+            if (!CanShoot())    continue;
+
+            // Attack telegraph -> let the Player know that this enemy is about to SHOOT
+            animator.SetBool("AttackTelegraph", true);
+            yield return new WaitForSeconds(0.5f);
+            animator.SetBool("AttackTelegraph", false);
 
             // Shoot
             Vector2 shootingVector = (GameManager.instance.PlayerPosition - (Vector2)transform.position).normalized;
-            Shoot(shootingVector, projectileSpeed, projectileLayer, projectileRange);   
+            Shoot(shootingVector, projectileSpeed, projectileLayer, projectileRange);
+
+            // Shooting cooldown
+            yield return new WaitForSeconds(shootingInterval);
         }
     }
 
@@ -80,5 +87,24 @@ public class Shooter : Enemy
             targetLayer,
             range
         );
+    }
+
+
+    /// <summary>
+    /// Check if the enemy is currently allowed to shoot.
+    /// </summary>
+    /// <returns>Whether the enemy is allowed to shoot.</returns>
+    private bool CanShoot() {
+        // False if can't shoot while running away and is currently running away
+        if (!canShootWhileRunningAway && myMovement.IsRunningAway) {
+            return false;
+        }
+        // False if can't shoot while moving and performing movement
+        if (!canShootWhileMoving && myMovement.IsMoving) {
+            return false;
+        }
+
+        // Otherwise true
+        return true;
     }
 }
