0868edd10c8cc15f890c21a48871e9df630f17bb
Sam :D <sebastiancc.info@gmail.com>
2023-04-25 20:00:20 +0200

    Create texture, preview sprite, and save image!

--- Assets/UI/Scripts/PixelEditor.cs
+++ Assets/UI/Scripts/PixelEditor.cs
@@ -17,25 +17,44 @@ public class PixelEditor : MonoBehaviour
     private string canvasID = "Canvas";
     [SerializeField]
     private string paletteContainerID = "PaletteContainer";
+    [SerializeField]
+    private string saveButtonID = "BtnSave";
+    [SerializeField]
+    private string spritePreviewID = "Preview";
 
     // Pixel Editor vars
-    private Color[ , ] pixelGrid = new Color[16, 16];
+    private int canvasSize = 16;
+    private PixelBlockUI[ , ] pixelGrid = new PixelBlockUI[16, 16];
     private Color selectedColor = Color.magenta;
+    private Texture2D drawingTex = null;
+
     public delegate void ColourDelegate(Color colour);
     public ColourDelegate SetColour;
-
 
     // UI Elements
     private Button pixelBlock = null;
     private VisualElement canvas = null;
     private VisualElement colourPalette = null;
+    private VisualElement spritePreview = null;
+    private Button saveButton = null;
 
 
     private void Start() {
+        // Initialise Variables
+        pixelGrid = new PixelBlockUI[canvasSize, canvasSize];
+        drawingTex = CreateTexture(true);
+        selectedColor = Palette.Colours[0];
+        
+        // Get UI Elements
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         canvas = root.Q<VisualElement>(canvasID);
         pixelBlock = root.Q<Button>(pixelBlockID);
         colourPalette = root.Q<VisualElement>(paletteContainerID);
+        spritePreview = root.Q<VisualElement>(spritePreviewID);
+        saveButton = root.Q<Button>(saveButtonID);
+
+        spritePreview.style.backgroundImage = drawingTex;
+        saveButton.clicked += SaveTexture;
 
         // pixelBlock.RegisterCallback<ClickEvent>(ChangeColour);
 
@@ -62,9 +81,10 @@ public class PixelEditor : MonoBehaviour
     /// Create the Pixel Grid in the canvas by adding Pixel elements to the Canvas and setting their callback events.
     /// </summary>
     private void CreatePixelGrid() {
-        for (int x = 0; x < pixelGrid.GetLength(0); x++) {
-            for (int y = 0; y < pixelGrid.GetLength(1); y++) {
-                Vector2Int position = new Vector2Int(x, y);
+        for (int y = 0; y < pixelGrid.GetLength(1); y++) {
+            for (int x = 0; x < pixelGrid.GetLength(0); x++) {
+                // Create new PixelBlock
+                Vector2Int position = new Vector2Int(x, canvasSize - (y + 1));
                 PixelBlockUI p = new PixelBlockUI(position, Color.clear);
 
                 // Callback Events
@@ -72,8 +92,11 @@ public class PixelEditor : MonoBehaviour
                 p.RegisterCallback<PointerEnterEvent>(OnPointerEnter, TrickleDown.TrickleDown);
                 p.RegisterCallback<PointerDownEvent>(OnPointerDown, TrickleDown.TrickleDown);
 
-
+                // Add to Canvas
                 canvas.Add(p);
+
+                // Add to 2D Array
+                pixelGrid[position.x, position.y] = p;
             }
         }
     }
@@ -90,7 +113,8 @@ public class PixelEditor : MonoBehaviour
         // target.Colour = Random.ColorHSV(hueMin:0f, hueMax:1f, saturationMin: 1f, saturationMax: 1f, valueMin:1f, valueMax:1f);
         
         // Debug prints
-        Debug.Log($"Clicked Pixel Block {target.name} @ {target.transform}");
+        // Debug.Log($"Clicked Pixel Block {target.name} @ {target.transform}");
+        Debug.Log($"Clicked Pixel Block {target.name} @ {target.Position}");
     }
 
 
@@ -131,12 +155,13 @@ public class PixelEditor : MonoBehaviour
 
 
     /// <summary>
-    /// Change the Colour of the clicked Pixel to the active colour (currently random)
+    /// Change the Colour of the clicked Pixel to the active colour
     /// </summary>
     /// <param name="pixel">The UI Pixel element in the canvas</param>
     private void ChangeColour(PixelBlockUI pixel) {
         pixel.Colour = selectedColor;
         Debug.Log($"Changed colour of pixel @ {pixel.Position} to {ColorUtility.ToHtmlStringRGBA(selectedColor)}");
+        UpdateTexture(pixel.Position, pixel.Colour);
     }
 
     /// <summary>
@@ -145,6 +170,7 @@ public class PixelEditor : MonoBehaviour
     /// <param name="pixel">The UI Pixel element in the canvas</param>
     private void ClearColour(PixelBlockUI pixel) {
         pixel.Colour = Color.clear;
+        UpdateTexture(pixel.Position, pixel.Colour);
     }
 
 
@@ -175,6 +201,52 @@ public class PixelEditor : MonoBehaviour
     /// <param name="newColour">New Colour to paint with.</param>
     private void SetSelectedColour(Color newColour) {
         selectedColor = newColour;
+    }
+
+
+    /// <summary>
+    /// Creates a Texture2D from scratch
+    /// </summary>
+    /// <returns>A Texture 2D object</returns>
+    private Texture2D CreateTexture(bool empty = false) {
+        // create empty texture of correct length
+        Texture2D tex = new Texture2D(pixelGrid.GetLength(0), pixelGrid.GetLength(1), TextureFormat.ARGB32, false);
+
+        // set point filter mode (no filtering)
+        tex.filterMode = FilterMode.Point;
+
+        // Set its colours
+        for (int y = 0; y < tex.height; y++) {
+            for (int x = 0; x < tex.width; x++) {
+                Color color = empty ? Color.clear : pixelGrid[x, y].Colour;
+                tex.SetPixel(x, y, color);
+            }
+        }
+
+        // Apply changes to texture
+        tex.Apply();
+        return tex;
+    }
+
+
+    /// <summary>
+    /// Updates the Drawing Texture in realtime!
+    /// </summary>
+    /// <param name="coordinates">Pixel coordinate to update.</param>
+    /// <param name="colour">New color to give that pixel.</param>
+    private void UpdateTexture(Vector2Int coordinates, Color colour) {
+        Debug.Log($"Updating Sprite Texture >>> {coordinates} -> {colour}");
+        drawingTex.SetPixel(coordinates.x, coordinates.y, colour);
+        drawingTex.Apply();
+    }
+
+
+    /// <summary>
+    /// Saves the drawn texture to disk -> CUSTOM folder
+    /// </summary>
+    private void SaveTexture() {
+        byte[] texBytes = drawingTex.EncodeToPNG();
+        System.IO.File.WriteAllBytes($"{ModManager.ModDirectory}/test.png", texBytes);
     }
 
 }
