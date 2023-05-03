918c52404a43fe92bfdec10dfe2fb39adf58d107
Sam :D <sebastiancc.info@gmail.com>
2023-05-03 20:45:02 +0200

    Created scripts for the new Pixel Editor
    Colour palette is now a separate script. cool :D

---
+++ Assets/UI/Scripts/ColourPaletteUI.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System;
+
+/// <summary>
+/// UI World Space Object that displays the Colour Palette on screen and sets the active selected colour.
+/// </summary>
+public class ColourPaletteUI : MonoBehaviour
+{
+    [Header("COLOUR PALETTE UI")]
+    [SerializeField]
+    private string colourSplashID = "ColourSplash";
+
+    // Vars
+    [SerializeField]
+    private Color selectedColour;
+
+    public delegate void ColourDelegate(Color colour);
+    public ColourDelegate OnClick;
+
+    public static event Action<Color> onColourChange;
+
+    // Elements
+    private VisualElement root = null;
+    List<VisualElement> elementList = null;
+    List<ColourSplash> splashList = new List<ColourSplash>();
+
+
+    /// <summary>
+    /// Helper UI Element representing each Colour in the Palette UI. It's a Visual Element containing a Button and Sprite.
+    /// <br>Invisible button that sets given Colour as active on click</br>
+    /// <br>Shows the Colour as an ink blob dynamically!</br>
+    /// </summary>
+    private class ColourSplash {
+        // Vars
+        private readonly Color colour;
+        private bool selected = false;
+
+        // Elements
+        private readonly VisualElement rootElement = null;
+        private readonly Button button = null;
+        private readonly MyImage sprite = null;
+
+
+        /// <summary>
+        /// Helper UI Element representing each Colour in the Palette UI. It's a Visual Element containing a Button and Sprite.
+        /// <br>Invisible button that sets given Colour as active on click</br>
+        /// <br>Shows the Colour as an ink blob dynamically!</br>
+        /// </summary>
+        /// <param name="element">UI Element in the UXML that this ColourSplash represents</param>
+        /// <param name="colour">The Colour from the Palette that this splash will represent</param>
+        /// <param name="onClick">Function to execute on click</param>
+        public ColourSplash(VisualElement element, Color colour, ColourDelegate onClick) {
+            // set properties
+            this.rootElement = element;
+            this.colour = colour;
+
+            // fetch elements
+            button = rootElement.Q<Button>();
+            sprite = rootElement.Q<MyImage>();
+
+            // set up Button
+            button.clicked += () => {
+                onClick?.Invoke(this.colour);
+            };
+
+            // Set up Sprite
+            sprite.sprite = sprite.style.backgroundImage.value.sprite;
+            sprite.tintColor = Color.green;
+            sprite.style.unityBackgroundImageTintColor = this.colour;
+        }
+    };
+
+
+    private void Start() {
+        // Initialise vars
+        SetColour(Palette.Colours[0]);
+        OnClick = SetColour;
+        
+        // Get Elements
+        root = GetComponent<UIDocument>().rootVisualElement;
+        elementList = root.Query<VisualElement>(colourSplashID).ToList();
+
+        // Initialise ColourSplash elements
+        elementList.ForEach(element => {
+            int i = elementList.IndexOf(element);
+            ColourSplash cs = new ColourSplash(element, Palette.Colours[i], OnClick);
+            splashList.Add(cs);
+        });
+    }
+
+    /// <summary>
+    /// Change the currently Selected Colour (i.e. the colour that that will be added to the canvas when clicked).
+    /// This is a delegate event called from the Child elements.
+    /// </summary>
+    /// <param name="newColour">New Colour to paint with.</param>
+    private void SetColour(Color newColour) {
+        selectedColour = newColour;
+        onColourChange?.Invoke(newColour);
+    }
+
+
+    // Public Getters
+    /// <summary>The currently selected colour to draw with.</summary>
+    public Color SelectedColour => selectedColour;
+}
---
+++ Assets/UI/Scripts/ColouringBook.cs
@@ -0,0 +1,204 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ColouringBook : MonoBehaviour
+{
+    [Header("COLOURING NOTEBOOK!")]
+
+    // UI Documents
+    [Header("Main UI Documents")]
+    [SerializeField][Tooltip("The UI Document of the Colour Palette")]
+    private UIDocument colourPaletteUI = null;
+    [SerializeField][Tooltip("The UI Document of the Notebook")]
+    private UIDocument notebookUI = null;
+    [SerializeField][Tooltip("The UI Document of the Paper")]
+    private UIDocument paperUI = null;
+
+    [Header("Element Names")]
+    [SerializeField]
+    private string canvasID = "Canvas";
+    [SerializeField]
+    private string saveButtonID = "BtnSave";
+    [SerializeField]
+    private string spritePreviewID = "Preview";
+
+    // Pixel Editor vars
+    private int canvasSize = 16;
+    private PixelBlockUI[ , ] pixelGrid = new PixelBlockUI[16, 16];
+    private Color selectedColor = Color.magenta;
+    private Texture2D drawingTex = null;
+
+    // Visual Elements
+    private VisualElement canvas = null;
+    private VisualElement spritePreview = null;
+    private Button saveButton = null;
+
+    // Components in Children
+    [Header("Child Scripts")]
+    [SerializeField]
+    private ColourPaletteUI colourPalette = null;
+
+
+    private void Awake() {
+        if (colourPalette == null) colourPalette = GetComponentInChildren<ColourPaletteUI>();
+    }
+
+
+    private void Start() {
+        // Initialise Variables
+        pixelGrid = new PixelBlockUI[canvasSize, canvasSize];
+        drawingTex = CreateTexture(true);
+
+        // Get UI Elements
+        canvas = notebookUI.rootVisualElement.Q<VisualElement>(canvasID);
+        spritePreview = paperUI.rootVisualElement.Q<VisualElement>(spritePreviewID);
+        saveButton = paperUI.rootVisualElement.Q<Button>(saveButtonID);
+
+        // Start Preview
+        spritePreview.style.backgroundImage = drawingTex;
+        saveButton.clicked += SaveTexture;
+
+        // Instantiate all Pixel blocks inside the canvas
+        CreatePixelGrid();
+    }
+
+
+    /// <summary>
+    /// Create the Pixel Grid in the canvas by adding Pixel elements to the Canvas and setting their callback events.
+    /// </summary>
+    private void CreatePixelGrid() {
+        canvas.Clear();
+
+        // Loop thru the Grid, creating a PixelBlock at every coordinate
+        for (int y = 0; y < pixelGrid.GetLength(1); y++) {
+            for (int x = 0; x < pixelGrid.GetLength(0); x++) {
+                // Create new PixelBlock
+                Vector2Int position = new Vector2Int(x, canvasSize - (y + 1)); // invert Y position
+                PixelBlockUI p = new PixelBlockUI(position, Color.clear);
+
+                // Callback Events
+                p.RegisterCallback<PointerEnterEvent>(OnPointerEnter, TrickleDown.TrickleDown);
+                p.RegisterCallback<PointerDownEvent>(OnPointerDown, TrickleDown.TrickleDown);
+
+                // Add to Canvas
+                canvas.Add(p);
+
+                // Add to 2D Array
+                pixelGrid[position.x, position.y] = p;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Triggers when clicking on a Pixel.
+    /// </summary>
+    private void OnPointerDown(PointerDownEvent evt) {
+        PixelBlockUI target = evt.target as PixelBlockUI;
+        // Debug.Log($"Clicked on [{target.name}] @ {target.worldBound}");
+
+        // Left Click
+        if (Input.GetMouseButton(0)) {
+            ChangeColour(target);
+        }
+        // Right Click
+        else if (Input.GetMouseButton(1)) {
+            ClearColour(target);
+        }
+    }
+
+    /// <summary>
+    /// Triggers when holding mouse over Pixel.
+    /// </summary>
+    private void OnPointerEnter(PointerEnterEvent evt) {
+        PixelBlockUI target = evt.target as PixelBlockUI;
+        // Debug.Log($"Hovering mouse over [{target.name}] @ {target.worldBound}");
+
+        // Left Click
+        if (Input.GetMouseButton(0)) {
+            ChangeColour(target);
+        }
+        // Right Click
+        else if (Input.GetMouseButton(1)) {
+            ClearColour(target);
+        }
+
+    }
+
+    /// <summary>
+    /// Change the Colour of the clicked Pixel to the active colour
+    /// </summary>
+    /// <param name="pixel">The UI Pixel element in the canvas</param>
+    private void ChangeColour(PixelBlockUI pixel) {
+        pixel.Colour = colourPalette.SelectedColour;
+        Debug.Log($"Changed colour of pixel @ {pixel.Position} to {ColorUtility.ToHtmlStringRGBA(colourPalette.SelectedColour)}");
+        UpdateTexture(pixel.Position, pixel.Colour);
+    }
+
+    /// <summary>
+    /// Erase the colour of the clicked Pixel (revert to original colour or transparent?)
+    /// </summary>
+    /// <param name="pixel">The UI Pixel element in the canvas</param>
+    private void ClearColour(PixelBlockUI pixel) {
+        pixel.Colour = Color.clear;
+        UpdateTexture(pixel.Position, pixel.Colour);
+    }
+
+
+    /// <summary>
+    /// Creates a Texture2D from scratch
+    /// </summary>
+    /// <returns>A Texture 2D object</returns>
+    private Texture2D CreateTexture(bool empty = false) {
+        // create empty texture of correct length
+        Texture2D tex = new Texture2D(pixelGrid.GetLength(0), pixelGrid.GetLength(1), TextureFormat.ARGB32, false);
+
+        // set point filter mode (no filtering)
+        tex.filterMode = FilterMode.Point;
+
+        // Set its colours
+        for (int y = 0; y < tex.height; y++) {
+            for (int x = 0; x < tex.width; x++) {
+                Color color = empty ? Color.clear : pixelGrid[x, y].Colour;
+                tex.SetPixel(x, y, color);
+            }
+        }
+
+        // Apply changes to texture
+        tex.Apply();
+        return tex;
+    }
+
+    /// <summary>
+    /// Updates the Drawing Texture in realtime!
+    /// </summary>
+    /// <param name="coordinates">Pixel coordinate to update.</param>
+    /// <param name="colour">New color to give that pixel.</param>
+    private void UpdateTexture(Vector2Int coordinates, Color colour) {
+        Debug.Log($"Updating Sprite Texture >>> {coordinates} -> {colour}");
+        drawingTex.SetPixel(coordinates.x, coordinates.y, colour);
+        drawingTex.Apply();
+    }
+
+    /// <summary>
+    /// Saves the drawn texture to disk -> CUSTOM folder
+    /// </summary>
+    private void SaveTexture() {
+        // Don't save the image if it has already been saved - kinda hacky way but it's only for now
+        if (saveButton.text == "SAVED!")    return;
+
+        // encode texture to byte array
+        byte[] texBytes = drawingTex.EncodeToPNG();
+
+        // Hash the bytes to generate a unique filenames - why not?
+        Hash128 hash = new Hash128();
+        hash.Append(texBytes);
+        
+        // Save bytes as a PNG in the CUSTOM/MyDrawings folder (for now)
+        System.IO.File.WriteAllBytes($"{ModManager.ModDirectory}/MyDrawings/{hash.ToString()}.png", texBytes);
+
+        // Update button text
+        saveButton.text = "SAVED!";
+    }
+}
