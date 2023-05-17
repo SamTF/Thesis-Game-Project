db3144034dee1f376a3ec3a0a14f05d02cd34cec
Sam :D <sebastiancc.info@gmail.com>
2023-05-17 19:49:30 +0200

    Added more options to the tile spawning system

---
+++ Assets/Scripts/Tiles/TilePlacer.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class that randomly spawns environment tiles in game world.
+/// </summary>
+public class TilePlacer : MonoBehaviour
+{
+    [Header("TILE PLACER")]
+    [SerializeField][Tooltip("Tile GameObject to spawn")]
+    private GameObject tilePrefab = null;
+    [SerializeField][Tooltip("Which sprites to use for tiles")]
+    private Sprite[] tiles = null;
+    [SerializeField][Tooltip("Color overlay to apply to all tiles")]
+    private Color tileColour = Color.white;
+    [SerializeField][Tooltip("Size of each square spawning sector")]
+    private int sectorSize = 8;
+    [SerializeField][Tooltip("How many sectors to spawn in each direction")]
+    private int radius = 4;
+
+
+    private void Awake() {
+        if (tilePrefab == null)
+            tilePrefab = Resources.Load<GameObject>("Tiles/Tile");
+    }
+
+    void Start() {
+        // Center the tiles in the middle of a sector, not an edge
+        transform.position = new Vector2(
+            -sectorSize / 2,
+            sectorSize / 2
+        );
+
+        // spawn the tiles!
+        SectorBased2();
+    }
+
+    private void SectorBasedLocal() {
+        for (int x = -radius; x < radius; x++) {
+            for (int y = -radius; y < radius; y++) {
+                bool addSector = Random.Range(1, 7) >= 4;
+
+                if (!addSector) continue;
+
+                GameObject sectorObj = new GameObject($"Sector ({x}, {y})");
+                Transform sector = sectorObj.transform;
+                sector.parent = transform;
+                sector.position = new Vector2(x * sectorSize, y * sectorSize);
+
+                Color sectorColour = Random.ColorHSV();
+
+                Sprite sectorTile = tiles[Random.Range(0, tiles.Length)];
+
+                for (int sx = 0; sx < sectorSize; sx++) {
+                    for (int sy = 0; sy < sectorSize; sy++) {
+                        bool addTile = Random.Range(1, 7) >= 5;
+                        if (!addTile)   continue;
+
+                        // Vector2 position = new Vector2( (x * sectorSize) + sx, (y * sectorSize) + sy );
+                        Vector3 position = new Vector3(sx, sy) + sector.position;
+
+                        GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, sector);
+                        tile.GetComponent<SpriteRenderer>().sprite = sectorTile;
+                        tile.GetComponent<SpriteRenderer>().color = tileColour;
+                        // tile.GetComponent<SpriteRenderer>().color = sectorColour;
+                    }
+                }
+            }
+        } 
+    }
+
+    /// <summary>
+    /// Spawn Tile Sectors with given tile properties, who themselves will spawn the tiles into the game world.
+    /// </summary>
+    private void SectorBased2() {
+        for (int x = -radius; x < radius; x++) {
+            for (int y = -radius; y < radius; y++) {
+                int spawnChance = Random.Range(0, 7) >= 5 ? 5 : 6;
+
+                Sprite sectorTile = tiles[Random.Range(0, tiles.Length)];
+
+                GameObject sectorObj = new GameObject($"Sector ({x}, {y})");
+                sectorObj.transform.parent = transform;
+                sectorObj.transform.position = new Vector2(x * sectorSize, y * sectorSize) + (Vector2)(transform.position);
+
+                TileSector sector = sectorObj.AddComponent<TileSector>();
+                sector.Init(tilePrefab, sectorTile, tileColour, sectorSize, spawnChance);
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Tiles/TileSector.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Helper class to concurrently spawn tiles randomly inside a grid.
+/// </summary>
+public class TileSector : MonoBehaviour
+{
+    private GameObject tilePrefab;
+    private Sprite tileSprite;
+    private Color tileColour;
+    private int sectorSize;
+    private int spawnChance;
+
+    /// <summary>
+    /// Initialise the Sector properties and begin spawning tiles.
+    /// </summary>
+    /// <param name="tilePrefab">GameObject of tile to spawn</param>
+    /// <param name="tileSprite">Sprite to assign to tile</param>
+    /// <param name="tileColour">Colour overlay to apply to tile sprite</param>
+    /// <param name="sectorSize">How big this sector is</param>
+    /// <param name="spawnChance">Dice roll required to spawn a tile (1 = 100% spawn chance, 6 = 16%)</param>
+    public void Init(GameObject tilePrefab, Sprite tileSprite, Color tileColour, int sectorSize, int spawnChance=5) {
+        // set properties
+        this.tilePrefab = tilePrefab;
+        this.tileSprite = tileSprite;
+        this.tileColour = tileColour;
+        this.sectorSize = sectorSize;
+        this.spawnChance = Mathf.Clamp(spawnChance, 0, 6);
+
+        // start placing tiles
+        StartCoroutine(PlaceTiles());
+    }
+
+    // Executed as a coroutine so several instances can run at the same time
+    private IEnumerator PlaceTiles() {
+        Debug.Log("PLACING TILES");
+        for (int x = 0; x < sectorSize; x++) {
+            for (int y = 0; y < sectorSize; y++) {
+                bool addTile = Random.Range(0, 7) >= spawnChance;
+
+                if (addTile) {
+                    Vector2 position = new Vector2(x, y) + (Vector2)transform.position;
+                    GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, transform);
+                    tile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+                    tile.GetComponent<SpriteRenderer>().color = tileColour;
+                }
+            }
+        }
+
+        yield return null;
+    }    
+}
