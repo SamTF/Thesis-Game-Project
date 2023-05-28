6159e0d637e1a4e8aca0412dfe06b99cde406d35
Sam :D <sebastiancc.info@gmail.com>
2023-05-28 18:21:12 +0200

    Added random spawner method

--- Assets/Enemies/EnemySpawner.cs
+++ Assets/Enemies/EnemySpawner.cs
@@ -8,24 +8,36 @@ public class EnemySpawner : MonoBehaviour
 {
     [System.Serializable]
     public class SpawnCode {
-        [SerializeField]
+        [SerializeField][Tooltip("GameObject of this enemy type.")]
         private GameObject prefab = null;
-        [SerializeField]
+        [SerializeField][Tooltip("Character representing this enemy in the spawn wave string.")]
         private char code;
+        [SerializeField][Tooltip("Minimum amount to spawn at once.")]
+        private int minAmount = 1;
+        [SerializeField][Tooltip("Maximum amount to spawn at once.")]
+        private int maxAmount = 1;
 
-        public SpawnCode(GameObject prefab, char code) {
+        public SpawnCode(GameObject prefab, char code, int minAmount = 1, int maxAmount = 1) {
             this.prefab = prefab;
             this.code = code;
+            this.minAmount = minAmount;
+            this.maxAmount = maxAmount;
         }
 
         public GameObject Prefab => prefab;
         public char Code => code;
+        public int MinAmount => minAmount;
+        public int MaxAmount => maxAmount;
     }
 
 
     [Header("ENEMY SPAWNER")]
     [SerializeField][TextArea(5,20)]
     private string spawnOrder = "";
+
+    [Header("Enemies")]
+    [SerializeField]
+    private SpawnCode[] enemies;
 
     [SerializeField]
     private SpawnCode chaser = null;
@@ -39,29 +51,32 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField][Tooltip("How far away enemies spawn from the player")][Range(10,20)]
     private int spawnDistance = 15;
 
+    [SerializeField][Tooltip("How long to wait when skipping (in seconds)")][Range(1, 10)]
+    private int skipTime = 10;
+
     private string[] spawnWaves;
     private Timer timer = null;
+    private Dictionary<char, SpawnCode> charToEnemy = new Dictionary<char, SpawnCode>();
 
 
     private void Start() {
+        // initialise dictionary
+        foreach (SpawnCode enemy in enemies) {
+            charToEnemy.Add(enemy.Code, enemy);
+        }
+
+        // start spawning enemies
         StartCoroutine(SpawnWave());
 
-        timer = new Timer(0);
-        timer.Start();
-
-        spawnWaves = spawnOrder.Split('\n');
-        
-        Debug.Log(spawnOrder);
-        foreach (string item in spawnWaves) {
-            Debug.Log(item);
-        }
+        // spawnWaves = spawnOrder.Split('\n');
+        // foreach (string item in spawnWaves) {
+        //     Debug.Log(item);
+        // }
     }
 
-    private void Update() {
-        // Debug.Log(timer.currentTime.String);
-    }
-
-
+    /// <summary>
+    /// Loop thru every character in the SpawnWave string and spawn the appropriate enemy or wait.
+    /// </summary>
     private IEnumerator SpawnWave() {
         foreach (char c in spawnOrder) {
             switch (c) {
@@ -83,7 +98,7 @@ public class EnemySpawner : MonoBehaviour
                 
                 default:
                     Debug.Log("skipping...");
-                    yield return new WaitForSeconds(10f);
+                    yield return new WaitForSeconds(skipTime);
                     break;
             }
         }
@@ -91,7 +106,36 @@ public class EnemySpawner : MonoBehaviour
         yield return null;
     }
 
+    private IEnumerator RandomSpawning() {
+        string enemyString = "ccssgb";
 
+        // continue spawning enemies while the player is still alive
+        while (Player.instance.Status.IsAlive) {
+            // rng should an enemy be spawned or not
+            if (Random.Range(0, 7) >= 4)
+                continue;
+
+            // fetch random enemy from the enemy string
+            int i = Random.Range(0, enemyString.Length-1);
+            char enemyChar = enemyString[i];
+
+            // error handling
+            if (charToEnemy.ContainsKey(enemyChar)) {
+                SpawnCode enemy = charToEnemy[enemyChar];
+                Spawn(enemy);
+            // default enemy if the given character doesn't exist
+            } else {
+                Spawn(chaser.Prefab);
+            }
+            
+            yield return new WaitForSeconds(skipTime/2f);
+        }
+    }
+
+    /// <summary>
+    /// Spawns a single enemy from a prefab.
+    /// </summary>
+    /// <param name="prefab">Enemy prefab to spawn.</param>
     private void Spawn(GameObject prefab) {
         Debug.Log($"[ENEMY SPAWNER] >>> Spawning {prefab.name}");
 
@@ -99,6 +143,20 @@ public class EnemySpawner : MonoBehaviour
 
         GameObject enemyObject = Instantiate(prefab, position, Quaternion.identity, transform);
         Enemy enemy = enemyObject.GetComponent<Enemy>();
+    }
+    /// <summary>
+    /// Spawn Enemy using its spawn code.
+    /// Spawns multiple enemies according to their Minimum and Maxmium amount values.
+    /// </summary>
+    /// <param name="spawnCode">Spawn Code</param>
+    private void Spawn(SpawnCode spawnCode) {
+        // spawn a random amount of enemies between the min and max values
+        int enemiesToSpawn = Random.Range(spawnCode.MinAmount, spawnCode.MaxAmount + 1);
+
+        for (int i = 0; i < enemiesToSpawn; i++) {
+            Vector2 position = (Random.insideUnitCircle.normalized * spawnDistance) + Player.instance.Position;
+            GameObject enemyObject = Instantiate(spawnCode.Prefab, position, Quaternion.identity, transform);
+        }
     }
 
 }
